2ce87445ec54e38ee99295384a3611f8433d16aa
Andrew Kesterson <andrew@aklabs.net>
2014-06-12 00:25:31 -0700

    Turned dynamic lights back on

--- moonlight/js/moonlight-skulk.js
+++ moonlight/js/moonlight-skulk.js
@@ -530,7 +530,7 @@ GameState.prototype.create = function()
 
     this.shadowTexture = game.add.bitmapData(game.world.width, game.world.height);
     // drop this lower to make the map darker
-    this.shadowTextureColor = 'rgb(30, 30, 30)';
+    this.shadowTextureColor = 'rgb(50, 50, 50)';
 
     // Create an object that will use the bitmap as a texture
     this.shadowSprite = game.add.image(0, 0, this.shadowTexture);
