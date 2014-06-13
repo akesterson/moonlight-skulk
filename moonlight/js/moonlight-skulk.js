7a6aeff7b08b479028bb396a59d7c650e4c86a28
Andrew Kesterson <andrew@aklabs.net>
2014-06-13 02:55:26 -0700

    Z index ordering didnt work, resize game to 800x600

--- moonlight/js/moonlight-skulk.js
+++ moonlight/js/moonlight-skulk.js
@@ -891,7 +891,7 @@ GameState.prototype.create = function()
     this.shadowTexture = game.add.bitmapData(game.world.width, game.world.height);
     // drop this lower to make the map darker
     //this.shadowTexturColor_Base = 30;
-    this.shadowTextureColor = 'rgb(50, 50, 50)';
+    this.shadowTextureColor = 'rgb(40, 40, 40)';
     //this.shadowTextureColor = 'rgb(255, 255, 255)';
 
     // Create an object that will use the bitmap as a texture
