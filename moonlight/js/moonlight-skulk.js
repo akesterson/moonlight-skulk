d11f46a592f2d6478f109e9972a58865a6f89ec2
Andrew Kesterson <andrew@aklabs.net>
2014-06-13 23:49:26 -0700

    Add positional sounds from the tilemap

--- moonlight/js/moonlight-skulk.js
+++ moonlight/js/moonlight-skulk.js
@@ -135,7 +135,7 @@ SoundSprite.prototype.adjust_relative_to = function(spr) {
 	);
     }
 
-    this.sound.volume = Math.min(1.0, Number(hyp_perfect / hyp));
+    this.sound.volume = Math.max(1.0, Number(hyp_perfect / hyp));
     console.log([hyp_perfect, hyp, Number(hyp_perfect / hyp), this.sound.volume]);
 }
 
