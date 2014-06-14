ff3e27e22530f897cf7353734ab38ad353d1a5a3
Andrew Kesterson <andrew@aklabs.net>
2014-06-14 10:46:50 -0700

    Go back to text wordbubbles

--- moonlight/js/moonlight-skulk.js
+++ moonlight/js/moonlight-skulk.js
@@ -766,7 +766,7 @@ var moonlightDialog = {
 function stringSize(str, font)
 {
     var f = font || '12px arial',
-    o = $('<div>' + this + '</div>')
+    o = $('<div>' + str + '</div>')
         .css({'position': 'absolute', 'float': 'left', 'white-space': 'nowrap', 'visibility': 'hidden', 'font': f})
         .appendTo($('body')),
     w = o.width(),
