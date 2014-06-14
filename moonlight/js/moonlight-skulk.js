4ad98f16a876668ff65f95161f80cd45c956963b
Andrew Kesterson <andrew@aklabs.net>
2014-06-13 22:48:58 -0700

    Add positional sounds from the tilemap

--- moonlight/js/moonlight-skulk.js
+++ moonlight/js/moonlight-skulk.js
@@ -894,8 +894,7 @@ GameState.prototype.preload = function()
     }
     for (var k in moonlightSettings['sounds']) {
 	var s = moonlightSettings['sounds'][k];
-	this.load.audio(s['name'], s['path']);
-	game.sound.add(s['name'], 1.0, false);
+	game.load.audio(s['name'], s['path']);
     }
     for (var k in moonlightSettings['spritesheets']) {
 	var s = moonlightSettings['spritesheets'][k]
