beb0d78a5ea766a198226558f5b7abb8156cdb41
Andrew Kesterson <andrew@aklabs.net>
2014-06-15 13:58:26 -0700

    Working on effect bubbles (visual indicators for awareness state changes)

--- moonlight/js/moonlight-skulk.js
+++ moonlight/js/moonlight-skulk.js
@@ -421,12 +421,12 @@ var moonlightSettings = {
 	    'loop': false
 	},
 	'relieved': {
-	    'frames': [47, 48, 49, 50, 51, 52, 53, 54],
+	    'frames': [48, 49, 50, 51, 52, 53, 54, 55],
 	    'speed': 2,
 	    'loop': false
 	},
 	'angry': {
-	    'frames': [55, 56, 57, 58, 59, 60, 61, 62],
+	    'frames': [56, 57, 58, 59, 60, 61, 62, 63],
 	    'speed': 2,
 	    'loop': false
 	},
