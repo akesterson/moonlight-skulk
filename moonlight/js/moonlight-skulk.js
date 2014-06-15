c7ae91fd15573a3ec37afb91dfa94011eaf394b1
Andrew Kesterson <andrew@aklabs.net>
2014-06-15 15:27:21 -0700

    Increase view size when alerted, go concerned instead of alerted when player is below the light threshold but within view

--- moonlight/js/moonlight-skulk.js
+++ moonlight/js/moonlight-skulk.js
@@ -705,9 +705,12 @@ var AISprite = function(game, x, y, key, frame) {
 
 	if ( hasState(this, state) == true ) {
 	    return;
-	} else if ( (state == STATE_LOSTHIM) && (hasState(this, STATE_ALERTED) == false)) {
+	} else if ( (state == STATE_LOSTHIM) && 
+		    (hasState(this, STATE_ALERTED) == false) &&
+		    (hasState(this, STATE_CONCERNED) == false) ) {
 	    return;
 	} 
+
 	setAwarenessState(this, state);
 
 	if ( this.awareness_effect !== null ) {
