ba34c9f3da592a5053ca1d1134964d183e05c67e
Andrew Kesterson <andrew@aklabs.net>
2014-06-15 19:28:19 -0700

    Size wordbubbles to text

--- moonlight/js/moonlight-skulk.js
+++ moonlight/js/moonlight-skulk.js
@@ -831,9 +831,8 @@ var AISprite = function(game, x, y, key, frame) {
 	this.bubble_sprite = game.add.sprite(this.x, this.y, 'wordbubble', bubblegrp);
 	this.bubble_sprite.anchor.setTo(0.5, 1.0);
 	console.log(this.bubble_sprite.scale);
-	this.bubble_sprite.scale = [Number(this.text_size[0] / bubbleimg.width),
-				    Number(this.text_size[1] / bubbleimg.height)
-				    ];
+	this.bubble_sprite.scale.x = Number(this.text_size[0] / bubbleimg.width);
+	this.bubble_sprite.scale.y = Number(this.text_size[1] / bubbleimg.height);
 	this.bubble_text = bubblegrp.add.text(this.x, this.y, text, style);
 	this.snap_bubble_position();
 
