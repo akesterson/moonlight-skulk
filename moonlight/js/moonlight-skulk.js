fa59e248c3180d5938e1d5986a2208279a82dc12
Andrew Kesterson <andrew@aklabs.net>
2014-06-15 20:44:33 -0700

    Add a preloader bar

--- moonlight/js/moonlight-skulk.js
+++ moonlight/js/moonlight-skulk.js
@@ -993,14 +993,7 @@ function addAnimation(obj, anim)
 GameState.prototype.preload = function()
 {
     game.load.image('preloader', 'gfx/ui/preloader.png');
-    while ( true ) {
-	try {
-	    if ( typeof game.cache.getImage('preloader') !== undefined )
-		break;
-	} catch(e) {
-	    continue;
-	}
-    }
+    while ( game.load.totalLoadedFiles() < 1 ) {}
     preloadBar = game.add.sprite(0, 0, 'preloader');
     console.log(preloadBar);
     preloadBar.anchor.setTo(0.5, 0.5);
