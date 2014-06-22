967a5a85a2df3820f25da66f33fa505a99c3b9d7
Andrew Kesterson <andrew@aklabs.net>
2014-06-22 10:34:42 -0700

    Fixed lingering static references to tiles in map.json

---
+++ moonlight/combine.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat src/Constants.js \
+    src/Settings.js \
+    src/Dialogue.js \
+    src/Util.js \
+    src/Light.js \
+    src/AISprite.js \
+    src/EffectSprite.js \
+    src/SoundSprite.js \
+    src/GameStates.js \
+    src/main.js > js/moonlight-skulk.js
